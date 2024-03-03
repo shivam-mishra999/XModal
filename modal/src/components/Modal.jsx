c991501fcffdffcfa9d7c9af3313fd865d80b980
Shivam <mishra.shiva999@gmail.com>
2024-03-03 20:46:05 +0530

    first commit

---
+++ modal/src/components/Modal.jsx
@@ -0,0 +1,88 @@
+import React, {useState, useEffect} from 'react';
+import "./Modal.css";
+
+
+export default function Modal(){
+    const [openForm, setOpenForm] = useState(false);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [dob, setDob] = useState("");
+
+    const handleOpenForm = (event) => {
+        console.log("opened")
+        event.stopPropagation();
+        setOpenForm(true);
+    }
+
+    const handleCloseForm = () => {
+        console.log("closed form")
+        setOpenForm(false);
+    }
+
+    const handleSubmit = () => {
+        if(!email.includes("@")){
+            alert(`Please include an '@' in the email address. ${email} is missing an '@'.` )
+            return;
+        }
+        if(phone.length !== 10){
+            alert("Invalid phone number. Please enter a 10-digit phone number.")
+            return;
+        }
+        const currentDate = new Date();
+        const inputDate = new Date(dob);
+        if(inputDate > currentDate){
+            alert("Invalid date of birth. Date of birth cannot be in the future.")
+        }
+        handleCloseForm();
+    }
+
+    useEffect(()=>{
+        const handleClickOutside = (event)=>{
+            const modalContent = document.querySelector('.modal-content');
+        
+            // Check if the clicked element is not a descendant of the modal content
+            if (openForm && !modalContent.contains(event.target)) {
+                handleCloseForm();
+            }
+        };
+        document.addEventListener("click", handleClickOutside);
+
+        return (()=>{
+            document.removeEventListener("click", handleClickOutside);
+        })
+
+    },[openForm])
+
+    return (
+        <div className='container'>  
+            <div className="modal">
+                <div>
+                    <h1>User Details Modal</h1>
+                    <button onClick={handleOpenForm}>Open Form</button>
+                </div>
+                
+
+                {openForm && (
+                <div className="modal-content">
+                <h2>Fill Details</h2>
+                <label htmlFor="username">Username:</label>
+                <input id="username" type="text" value={username} onChange={(e)=>setUsername(e.target.value)} required />
+
+                <label htmlFor="email">Email Address:</label>
+                <input id="email" type="text" value={email} onChange={(e)=> setEmail(e.target.value)} required />
+
+                <label htmlFor="phone">Phone Number:</label>
+                <input id="phone" type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} required />
+
+                <label htmlFor="dob">Date of Birth:</label>
+                <input id="dob" type="date" value={dob} onChange={(e)=>setDob(e.target.value)} required />
+
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+            )}
+                
+            </div>
+        </div>
+    )
+};
